339d8dc1670f04f69aa7c06b874980efe5ba44ec
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-01 20:01:57 -0300

    env: setting url env

--- server/keys.js
+++ server/keys.js
@@ -3,5 +3,6 @@ module.exports = {
 	secret:process.env.SECRET,
 	mgHost: process.env.HOST,
 	mgDatabase: process.env.MONGO_INITDB_DATABASE,
-	mgPort: process.env.ME_CONFIG_MONGODB_PORT
+	mgPort: process.env.ME_CONFIG_MONGODB_PORT,
+	url:"https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
 };
