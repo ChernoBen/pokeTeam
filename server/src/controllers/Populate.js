475bece72efa5ccd9a8d8ca6ac02fdb7085991e3
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-02 17:10:28 -0400

    fix: verify duplicity

--- server/src/controllers/Populate.js
+++ server/src/controllers/Populate.js
@@ -3,10 +3,13 @@ const data = require("../utils/dataset");
 
 class PokeToolController{
     async populate(req,res){
-        console.log(data[0])
         data.map(async poke =>{
-            let newPokemon = Pokemon({name:poke.name,ref:poke.url});
-            await newPokemon.save();
+            let found = await Pokemon.findOne({name:poke.name})
+            if(!found){
+                let newPokemon = Pokemon({name:poke.name,ref:poke.url});
+                await newPokemon.save();
+            }
+            
         });
         return res.status(200).json(data[0])
     }
