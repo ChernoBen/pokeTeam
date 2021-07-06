1d8b69f1c539363e935768be8ac3585ccd3920a0
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-06 11:58:41 -0400

    test: setting 90ms timeout

--- server/test/user.test.js
+++ server/test/user.test.js
@@ -7,12 +7,28 @@ const axios = require("axios");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
-
+setTimeout(()=>{console.log("Waiting for the worker start")},90000)
 const request = supertest(app.listen(3002,async () => {
 	console.log("User tests...");
-	const worker = await axios.get("http://localhost:5000/worker");
+	try{
+        const worker = await axios.get("http://worker:5000/worker");
+        setTimeout(()=>{console.log("Waiting for the worker response")},90000)
+        if(worker){
+            if(!worker.message){
+                if(worker.data["local-count"] == worker.data["web-count"]){
+                    console.log("Data base is updated");
+                }else{
+                    console.log(`Pokemons on database:${worker.data["local-count"]}\n Pokemons on web:${worker.data["web-count"]}`);
+                }
+            }else{
+                console.log(worker.message);
+            }
+            
+        }      
+    }catch(error){
+        console.log("Error while pulling the worker trigger")
+    }
 }));
-
 let mainUser = {
     name:`Name-${Date.now()}`,
     email:`email${Date.now()}@gmail.com`,
